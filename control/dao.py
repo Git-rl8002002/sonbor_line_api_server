5fa454289c00b2f8eeeb47a4909606afefaf68c0
Git-rl8002002 <139932764+Git-rl8002002@users.noreply.github.com>
2025-04-10 11:31:45 +0800

    Add files via upload

---
+++ control/dao.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Date        : 20250407
+# Author      : Jason Hung
+# Version     : V1.0
+# Description : SonBor Line Messaging API 
+
+para = {'line_bot_api' : '/Cnorb4qfJULKMlvrO9RPPhWNk/jrlArOm0T6I3P/B5Er5x+bwUJ4A8vdOzUFM+cDnZF/GPdGEZx5lHnVM180363k15zOedERjYFz5f0itcIxADCquDM/o1hjKAWCLm/l5m0+G5/wCgEJ9to8LivHwdB04t89/1O/w1cDnyilFU=' ,
+        'handler' : 'cf22f3a7026c0643b5e6aa4891ed761d'
+        }
+
+
